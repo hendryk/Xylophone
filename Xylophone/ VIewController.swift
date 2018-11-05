3b6aacf614e5699971827094d6105f0a1ad2c24d
Jacek Krzyżanowski <krzyzakov@gmail.com>
2018-11-05 22:26:31 +0100

    Pierwszy sposób

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -1,17 +1,26 @@
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController{
     
+    var player: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     @IBAction func notePressed(_ sender: UIButton) {
-        print(sender.tag)
+        let xylophoneSound = Bundle.main.url(forResource: "note1", withExtension: "wav")!
+        do {
+            player = try AVAudioPlayer(contentsOf: xylophoneSound)
+            guard let player = player else {return}
+            player.prepareToPlay()
+            player.play()
+            
+        } catch let error as Error {
+            print(error)
+        }
     }
-    
-  
 
 }
 
